cea7324cb8d52d7d261331ba09c73d9bc7858ae5
Dawson <scheufeled@dewv.edu>
2018-09-05 16:45:11 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,1 +1,33 @@
+import { game, Sprite } from "./sgc/sgc.js";
 
+game.setBackground("floor.png");
+game.showScore = true;
+
+class PlayerWizard extends Sprite {
+    constructor() {
+        super();
+        this.name = "Marcus the Wizard";
+        this.setImage("marcusSheet.png");
+        this.width = 48;
+        this.height = 48;
+        this.x = this.width;
+        this.y = this.y;
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up", 6, 8);
+        this.speedWhenWalking = 100;
+        
+       
+    } 
+    handleDownArrowKey() {
+        this.playAnimation("down");
+        this.speed = this.speedWhenWalking;
+        this.angle = 270;
+    }
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
+    }
+}
+
+let marcus = new PlayerWizard();
