3422aee3718fe7874bc8aa738225354552591a4d
Dawson <scheufeled@dewv.edu>
2018-09-12 16:08:48 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -16,18 +16,43 @@ class PlayerWizard extends Sprite {
         this.defineAnimation("up", 6, 8);
         this.speedWhenWalking = 100;
         
-       
     } 
     handleDownArrowKey() {
         this.playAnimation("down");
         this.speed = this.speedWhenWalking;
         this.angle = 270;
     }
-    handleUpArrowKey() {
-        this.playAnimation("up");
-        this.speed = this.speedWhenWalking;
-        this.angle = 90;
-    }
-}
+   handleUpArrowKey() {
+       this.playAnimation("up");
+       this.speed = this.speedWhenWalking;
+       this.angle = 90;
+   }
+   handleGameLoop () { // Keep Marucs in the display area
+        this.y = Math.max(5, this.y);
+        this.y = Math.min(552, this.y);
+   }
+}   
 
 let marcus = new PlayerWizard();
+
+class Spell extends Sprite {
+    constructor() {
+        this.speed = 200;
+        this.height = 48;
+        this.width = 48;
+        this.defineAnimation("magic", 0, 7);
+        this.playAnimation("magic", true);
+    }
+    handleSpaceBar() {
+        let spell = new Spell();
+        spell.x = this.x;  // this sets the position of the spell object equal to
+        spell.y = this.y;  // the position of any object created from the PlayerWizard class
+        this.name = "A spell cast by Marcus";
+        this.setImage("marcusSpellSheet.png");
+        this.angle = 0;
+    }
+    handleBoundaryContract() {
+        //Delete spell when it leaves display area
+        game.removeSprite(this);
+    }
+}
